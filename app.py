92a6898153bc58b90448a3b0e79724e22c07eaae
Sandra  Betancur <smbaestudio@gmail.com>
2024-11-21 20:47:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ Original file is located at
 
 """## Examen Final 20%
 
+---
+*Sandra Milena Betancur A.*
+*Leidy Tatiana Castañeda.*
+
+
 Descripción del Proyecto:
 El objetivo de este trabajo es que los estudiantes apliquen los conocimientos adquiridos durante el curso para trabajar con herramientas modernas de desarrollo y despliegue de aplicaciones web. Para ello, deberán:
 
